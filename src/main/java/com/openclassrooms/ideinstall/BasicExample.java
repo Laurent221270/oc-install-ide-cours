c7292916456fee9b07844c990144b3d597df9665
Buisson <Buisson@Buisson-PC>
2023-09-03 10:49:30 +0200

    ajout super traitement

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -8,5 +8,12 @@ public class BasicExample {
 			System.out.println("Hello bugs !");
 			i++;
 		}
+		
+		int[] tab= {101,202,303,404,505,605};
+		
+		for (int j = 0; j < tab.length; j++) {
+			System.out.println("tab["+j+"]="+tab[j]);			
+		} 
+		
 	}
 }
